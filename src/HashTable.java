04dabbed169a55d582f86b5e31951b584639c2f6
zhasulan31-0 <76615408+zhasulan31-0@users.noreply.github.com>
2024-06-11 17:09:32 +0500

    Extending class methods

---
+++ src/HashTable.java
@@ -0,0 +1,132 @@
+import java.util.Random;
+
+public class MyHashTable<K, V> {
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+    }
+
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+    }
+
+    private int hash(K key) {
+        return Math.abs(key.hashCode() % M);
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                node.value = value;
+                return;
+            }
+            node = node.next;
+        }
+
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        while (node != null) {
+            if (node.key.equals(key)) {
+                return node.value;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> node = chainArray[index];
+        HashNode<K, V> prev = null;
+        while (node != null) {
+            if (node.key.equals(key)) {
+                if (prev == null) {
+                    chainArray[index] = node.next;
+                }
+                else {
+                    prev.next = node.next;
+                }
+                size--;
+                return node.value;
+            }
+            prev = node;
+            node = node.next;
+        }
+        return null;
+    }
+
+    public boolean contains(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return true;
+                }
+                node = node.next;
+            }
+        }
+        return false;
+    }
+
+    public K getKey(V value) {
+        for (HashNode<K, V> node : chainArray) {
+            while (node != null) {
+                if (node.value.equals(value)) {
+                    return node.key;
+                }
+                node = node.next;
+            }
+        }
+        return null;
+    }
+
+/**    public static void main(String[] args) {
+        MyHashTable<MyTestingClass, String> table = new MyHashTable<>();
+        int buckets = 11; // Количество бакетов в хеш-таблице
+        int[] bucketSizes = new int[buckets];
+
+        // Добавление 10000 случайных элементов в хеш-таблицу
+        for (int i = 0; i < 10000; i++) {
+            MyTestingClass key = new MyTestingClass(RANDOM.nextInt(10000));
+            String value = "Student" + i;
+            table.put(key, value);
+
+            // Увеличение размера соответствующего бакета
+            int index = key.hashCode() % buckets;
+            bucketSizes[index]++;
+        }
+
+        // Вывод количества элементов в каждом бакете
+        for (int i = 0; i < buckets; i++) {
+            System.out.println("Bucket " + i + ": " + bucketSizes[i] + " elements");
+        }
+    }**/
+}
